c4a214bbe6d072d98d4b45839202d432feac80d1
abu0127 <abu/272001>
2025-09-18 19:01:54 +0300

    add

--- src/App.jsx
+++ src/App.jsx
@@ -81,7 +81,7 @@ function Card({ item, onClick }) {
       }}
     >
       <img
-        src={item.image}
+        src={`${import.meta.env.BASE_URL}${item.image}`}
         alt={item.name}
         style={{
           width: "100%",
@@ -162,7 +162,7 @@ function Modal({ item, onClose }) {
         </button>
 
         <img
-          src={item.image}
+          src={`${import.meta.env.BASE_URL}${item.image}`}
           alt={item.name}
           style={{
             width: "100%",
